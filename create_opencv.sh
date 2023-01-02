4eaab5732ba9c8cea7c1b4f4fd3a4fcc36243a64
Abhishek Thakur <abhi.una12@gmail.com>
2023-01-02 13:50:49 +0530

    [RELEASE] Fixed gstreamer installation

--- create_opencv.sh
+++ create_opencv.sh
@@ -42,7 +42,7 @@ sudo apt-get install -y -qq --allow-unauthenticated zlib1g-dev libjpeg-dev check
 
 sudo apt-get install -y -qq --allow-unauthenticated libgstreamer1.0-0 libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev 
 
-sudo apt-get install -y -qq --allow-unauthenticated gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+sudo apt-get install -y -qq --allow-unauthenticated gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-opencv gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 echo "Installing OpenCV Library"
 
