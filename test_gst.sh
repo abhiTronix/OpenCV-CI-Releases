3f160ef53427830e98a9efafd47f1aaedf0956f5
abhiTronix <abhi.una12@gmail.com>
2025-06-15 14:54:27 +0530

    Remove set -e from GStreamer test script to prevent premature exit on errors

--- test_gst.sh
+++ test_gst.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
 echo "Testing GStreamer configuration..."
 
 dpkg --get-selections | grep hold
