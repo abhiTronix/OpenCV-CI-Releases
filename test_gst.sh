9e13299ea0d51551d094b74e8f991c701e5977d6
abhiTronix <abhi.una12@gmail.com>
2025-06-15 15:18:17 +0530

    Changes

--- test_gst.sh
+++ test_gst.sh
@@ -14,13 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 echo "Testing GStreamer configuration..."
 
 dpkg --get-selections | grep hold
 
 sudo apt-get install -y -qq --allow-unauthenticated build-essential gfortran cmake
-
-sudo apt-get install -y -qq --allow-unauthenticated libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev 
 
 cd test_gstreamer || true
 
