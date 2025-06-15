349d44c6ab2a52afaf839ce46a1f0e0aa0a92391
abhiTronix <abhi.una12@gmail.com>
2025-06-15 14:39:36 +0530

    Changes

--- test_gst.sh
+++ test_gst.sh
@@ -18,6 +18,8 @@ set -e
 
 echo "Testing GStreamer configuration..."
 
+dpkg --get-selections | grep hold
+
 sudo apt-get install -y -qq --allow-unauthenticated build-essential gfortran cmake
 
 sudo apt-get install -y -qq --allow-unauthenticated libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev 
